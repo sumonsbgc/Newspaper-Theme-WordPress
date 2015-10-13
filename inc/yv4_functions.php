caa78400cdc41f3a55b686d2db97d6d97013534a
Chris King <cjk508@gmail.com>
2015-10-14 00:55:27 +0100

    Resolve #3
    Only one of the categories will be shown now

---
+++ inc/yv4_functions.php
@@ -0,0 +1,14 @@
+<?php
+function yv4_the_category() {
+    $cats = get_the_category(); // category object
+    $top_cat_obj = array();
+
+    foreach($cats as $cat) {
+        if ($cat->parent == 0) {
+            $top_cat_obj[] = $cat;
+        }
+    }
+    $top_cat_obj = $top_cat_obj[0];
+    return $top_cat_obj->name;
+}
+?>
