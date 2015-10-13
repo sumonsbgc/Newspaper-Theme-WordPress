0550ac003a60b7888184e8f82a7a06c14a817de5
Chris King <cjk508@gmail.com>
2015-10-14 00:57:42 +0100

    Resolve #1 - Colours updated
    Updated colours so they are different for each category

--- functions.php
+++ functions.php
@@ -152,3 +152,5 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+require get_template_directory() . '/inc/yv4_functions.php';
