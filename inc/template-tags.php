4366300a7edb89e06b4df67bd4d015c126ededc0
Chris King <cjk508@gmail.com>
2015-10-14 21:22:50 +0100

    Resolve #10  - Title bar updated
    Still need to add social links

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -25,8 +25,7 @@ function yv4_posted_on() {
 	);
 
 	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'yv4' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+		esc_html_x( 'Posted on %s', 'post date', 'yv4' ),$time_string
 	);
 
 	$byline = sprintf(
@@ -34,7 +33,7 @@ function yv4_posted_on() {
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+	echo '<date class="posted-on">' . $posted_on . '</date>';//<span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
 
 }
 endif;
--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -10,13 +10,30 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	<header class = "articleHeader">
+	    <h1 class = "articleTitle"><?php the_title(); ?> </h1>
+	    <div class = "article-meta">
+	        <span id = 'author'><i class="fa fa-2x fa-user"></i> <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">
+					<?php the_author(); ?></a>
+			</span>
+	        <span id = "date"><i class="fa fa-2x fa-clock-o"></i><?php yv4_posted_on();?></span>
+	        <span id = "social">
+	            <i class="fa fa-2x fa-twitter"></i>
+	            <i class="fa fa-2x fa-facebook"></i>
+	        </span>
+	    </div>
+	</header>
 
-		<div class="entry-meta">
-			<?php yv4_posted_on(); ?>
-		</div><!-- .entry-meta -->
-	</header><!-- .entry-header -->
+
+	<?php if (has_post_thumbnail( $post->ID ) ): ?>
+	  <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'articleImage' );
+	  	if($image[1] >= 1050 && ($image[2] > 300 && $image[2]<500 )):
+	  ?>
+		<div class = "fullImageWrapper">
+			<img class = "full-size" src = "<?php echo $image[0] ?>">
+		</div>
+	<?php endif;
+	endif; ?>
 
 	<div class="entry-content">
 		<?php the_content(); ?>
@@ -32,4 +49,3 @@
 		<?php yv4_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
-
