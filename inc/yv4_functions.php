aa35f871b7cbf21cdba60c4637c893effd301184
Chris King <cjk508@gmail.com>
2015-10-17 21:55:27 +0100

    Null protection

--- inc/yv4_functions.php
+++ inc/yv4_functions.php
@@ -8,8 +8,11 @@ function yv4_the_category() {
             $top_cat_obj[] = $cat;
         }
     }
-    $top_cat_obj = $top_cat_obj[0];
-    return $top_cat_obj->name;
+    if (sizeof($top_cat_obj) >= 1 ){
+        $top_cat_obj = $top_cat_obj[0];
+        return $top_cat_obj->name;
+    }
+    return "Uncategorised";
 }
 add_filter( 'jetpack_development_mode', '__return_true' );
 ?>
