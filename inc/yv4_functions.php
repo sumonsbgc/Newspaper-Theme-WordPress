9b5863171361663c8f03254ba1a75a979ac99d8c
Chris King <cjk508@gmail.com>
2015-10-17 22:12:15 +0100

    Updated Comments features

--- inc/yv4_functions.php
+++ inc/yv4_functions.php
@@ -1,5 +1,5 @@
 <?php
-function yv4_the_category() {
+function yv4_the_parent_category() {
     $cats = get_the_category(); // category object
     $top_cat_obj = array();
 
@@ -8,22 +8,49 @@ function yv4_the_category() {
             $top_cat_obj[] = $cat;
         }
         else{
-            $parents = get_category_parents($cat->term_id);
-            foreach($parents as $parent) {
-                if ($parent->parent == 0) {
-                    $top_cat_obj[] = $parents;
-                }
-            }
+            $top_cat_obj[] = yv4_category_top_parent_id($cat->term_id);
         }
     }
     if (sizeof($top_cat_obj) >= 1 ){
         $top_cat_obj = $top_cat_obj[0];
         return $top_cat_obj->name;
     }
-    else{
-
-    }
     return "Uncategorised";
 }
 add_filter( 'jetpack_development_mode', '__return_true' );
+
+function yv4_the_category() {
+    $cats = get_the_category(); // category object
+    $top_cat_obj = array();
+
+    foreach($cats as $cat) {
+        $top_cat_obj[] = $cat;
+    }
+    if (sizeof($top_cat_obj) >= 1 ){
+        return $top_cat_obj;
+    }
+    return "Uncategorised";
+}
+
+/**
+* Returns ID of top-level parent category, or current category if you are viewing a top-level
+*
+* @param	string		$catid 		Category ID to be checked
+* @return 	category	$catParent	 top-level parent category
+*/
+
+function yv4_category_top_parent_id ($catid) {
+
+     while ($catid) {
+          $cat = get_category($catid); // get the object for the catid
+          $catid = $cat->category_parent; // assign parent ID (if exists) to $catid
+          // the while loop will continue whilst there is a $catid
+          // when there is no longer a parent $catid will be NULL so we can assign our $catParent
+          $catParent = $cat->cat_ID;
+          $catParent =  get_category($cat->cat_ID) ;
+     }
+
+    return $catParent;
+}
+
 ?>
--- template-parts/content-card.php
+++ template-parts/content-card.php
@@ -7,7 +7,7 @@
             <div class="card-content">
                 <h2><a href="<?php the_permalink();?>"> <?php the_title();?></a></h2>
                 <div class="card-cat">
-                    <i class="triangle <?php echo strtolower(yv4_the_category());?>"></i><?php echo yv4_the_category();?>
+                    <i class="triangle <?php echo strtolower(yv4_the_parent_category());?>"></i><?php echo yv4_the_category()[0]->name;?>
                 </div>
             </div>
         </div>
