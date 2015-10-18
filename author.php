934d4f07f8730aeb9e465fe09d9876a94a9334b4
Chris King <cjk508@gmail.com>
2015-10-18 13:21:35 +0100

    Fixed php error

--- author.php
+++ author.php
@@ -1,9 +1,5 @@
 <?php
 get_header();
-            // Set author data.
-$curauth = (isset($_GET['author_name'])) ?
-        get_user_by('slug', $author_name) : get_userdata(intval($author));
-$numPosts = count_user_posts($curauth->ID);
 ?>
 <div class = "article-Column">
     <?php get_template_part("template-parts/author", "box"); ?>
--- template-parts/author-box.php
+++ template-parts/author-box.php
@@ -1,3 +1,8 @@
+<?php 
+$curauth = (isset($_GET['author_name'])) ?
+        get_user_by('slug', $author_name) : get_userdata(intval($author));
+$numPosts = count_user_posts($curauth->ID);
+?>
 <div class = "card-row author-box">
     <div class = "avatar">
         <?php echo get_avatar( get_the_author_meta("email"), '128',null, get_the_author() ); ?>
